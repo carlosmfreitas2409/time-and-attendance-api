4b4befe9c92037b31626bf3768a48ff64e2b975c
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2021-07-01 18:20:19 -0300

    :goal_net: chore: add validation and error handler

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import 'express-async-errors';
 
 import './database';
 import './shared/container';
 
 import { router } from './routes';
+import { globalErrorHandler } from './shared/middlewares/globalErrorHandler';
 
 const app = express();
 
 app.use(express.json());
 app.use('/v1', router);
+
+app.use(globalErrorHandler);
 
 app.listen(3333, () => {
   console.log('🚀 Server running on PORT 3333!');
---
+++ src/shared/middlewares/bodyValidation.ts
@@ -0,0 +1,25 @@
+import { transformAndValidate, ClassType } from 'class-transformer-validator';
+import { NextFunction, Request, Response, RequestHandler } from 'express';
+
+import { formatErrors } from '../../utils/formatErrors';
+
+export function bodyValidation(
+  type: ClassType<Record<string, any>>,
+): RequestHandler {
+  return async (
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
+    try {
+      await transformAndValidate(type, request.body);
+      return next();
+    } catch (errors) {
+      return response.status(400).json({
+        status: 400,
+        message: 'Validation failed',
+        errors: formatErrors(errors),
+      });
+    }
+  };
+}
