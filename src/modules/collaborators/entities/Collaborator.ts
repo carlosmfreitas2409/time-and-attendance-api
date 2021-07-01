62e833c4597dd4544c143df9f2f1ce64f3e4ef1b
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2021-07-01 18:24:09 -0300

    :sparkles: feat: add collaborators database and repository

---
+++ src/modules/collaborators/entities/Collaborator.ts
@@ -0,0 +1,58 @@
+import { Expose } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Skill } from './Skill';
+
+@Entity('collaborators')
+class Collaborator {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  cpf: number;
+
+  @Column({ nullable: true })
+  phone_number: string;
+
+  @ManyToMany(() => Skill)
+  @JoinTable({
+    name: 'collaborator_skills',
+    joinColumn: {
+      name: 'collaborator_id',
+    },
+    inverseJoinColumn: {
+      name: 'skill_id',
+    },
+  })
+  skills: Skill[];
+
+  @Column()
+  validated: boolean;
+
+  @Column()
+  validated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Expose({ name: 'matriculation' })
+  getMatriculation(): string {
+    const userID = String(this.id);
+    return userID.padStart(4, '0');
+  }
+}
+
+export { Collaborator };
