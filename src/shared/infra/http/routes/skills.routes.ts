d1c94a554b44f90154a8590a040d81d985ca2f5b
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2021-07-02 02:41:45 -0300

    :lock: fix: add authenticated and admin middleware to skills routes

--- src/shared/infra/http/routes/skills.routes.ts
+++ src/shared/infra/http/routes/skills.routes.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import { CreateSkillController } from '@modules/collaborators/useCases/createSkill/CreateSkillController';
 import { DeleteSkillController } from '@modules/collaborators/useCases/deleteSkill/DeleteSkillController';
 import { ListSkillsController } from '@modules/collaborators/useCases/listSkills/ListSkillsController';
+import { ensureAdmin } from '@modules/users/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@modules/users/middlewares/ensureAuthenticated';
 
 const skillsRouter = Router();
 
@@ -11,7 +13,19 @@ const createSkillController = new CreateSkillController();
 const deleteSkillController = new DeleteSkillController();
 
 skillsRouter.get('/', listSkillsController.handle);
-skillsRouter.post('/', createSkillController.handle);
-skillsRouter.delete('/:id', deleteSkillController.handle);
+
+skillsRouter.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createSkillController.handle,
+);
+
+skillsRouter.delete(
+  '/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  deleteSkillController.handle,
+);
 
 export { skillsRouter };
