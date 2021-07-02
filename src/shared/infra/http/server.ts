ffc9ea8f390f1e724e8bb05de883d0792e67d00f
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2021-07-02 02:33:18 -0300

    :memo: feat: add Swagger documentation

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,17 +2,22 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 import 'express-async-errors';
 
 import '../typeorm';
 import '@shared/container';
 
+import swaggerFile from '../../../swagger.json';
 import { globalErrorHandler } from './middlewares/globalErrorHandler';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
 app.use('/v1', router);
 
 app.use(globalErrorHandler);
